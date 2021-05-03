40fad62d1cf46ae3e3c3ce947fea26f3b9ee333f
Changemin <bkm.change.min@gmail.com>
2021-05-03 20:52:32 +0900

    Add 'comment'

--- Sources/CMKit/ViewModifiers/ResizableImage.swift
+++ Sources/CMKit/ViewModifiers/ResizableImage.swift
@@ -7,6 +7,20 @@
 
 import SwiftUI
 
+/**
+ 이미지를 늘릴 수 있게 하고, templace rendering image로 변환하여 다른 색을 입힙니다.
+ 
+ - Parameters:
+     - width: 사진의 가로
+     - height: 사진의 높이
+     - color: 사진 색
+ 
+    
+ # 사용예시 #
+ ```
+ Image("test").resizableImage(wdith: 30, heigiht: 30, .red)
+ ```
+ */
 extension Image {
     public func resizableImage(width: CGFloat, height: CGFloat, _ color: Color) -> some View {
         self.renderingMode(.template)
