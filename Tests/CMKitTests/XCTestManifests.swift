bc99af6e04531f55b4e2da2f2177ed2086bedb5d
Changemin <bkm.change.min@gmail.com>
2021-05-03 20:12:03 +0900

    Remove 'tests'

--- Tests/CMKitTests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(CMKitTests.allTests),
-    ]
-}
-#endif
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import CMKitTests
-
-var tests = [XCTestCaseEntry]()
-tests += CMKitTests.allTests()
-XCTMain(tests)
